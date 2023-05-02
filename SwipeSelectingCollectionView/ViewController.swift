36636e1df96544242f719fcc2ab38a6f7cf71ed0
balabon7 <balabon7@gmail.com>
2023-05-02 13:06:24 +0300

    Initial Commit

---
+++ SwipeSelectingCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeSelectingCollectionView
+//
+//  Created by Oleksandr Balabon on 02.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
